696fb7096d40ce9fbe0b540a75756ec6a6269c2d
Aeoliun <aeoliun@gmail.com>
2021-08-01 19:22:37 -0400

    changed c++ to c

--- Problem_1/main.c
+++ Problem_1/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int returnMultiples() {
+int multiples() {
     int sum = 0;
 
     for (int i = 0; i < 1000; i++) {
-        if (i % 5 == 0 || i % 3 == 0) {
+        if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
     }
+
     return sum;
 }
 
 int main() {
-
-    int sum = returnMultiples();
-
+    
+    int sum;
+    sum = multiples();
     printf("%d\n", sum);
 
     return 0;
